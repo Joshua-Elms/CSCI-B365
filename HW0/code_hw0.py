6de1231320c2625d85fde4fd31a9755dc442562a
Joshua-Elms <bacon.joshy@gmail.com>
2022-01-21 13:36:35 -0500

    Completed function to plot teach$Ratio; p1 done

--- HW0/code_hw0.py
+++ HW0/code_hw0.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def q1_1(teach_path: str) -> pd.DataFrame:
     """
@@ -16,7 +18,31 @@ def q1_2(df0: pd.DataFrame) -> list:
     return list(df0["Ratio"])
 
 
+def q1_3(df0: pd.DataFrame) -> pd.DataFrame:
+    """
+    Selects rows that have "Ratio" values 18 < val < 22
+    """
+    return df0[(df0["Ratio"] > 18) & (df0["Ratio"] < 22)]
+
+
+def q1_4(df0: pd.DataFrame):
+    """
+    Plots and displays a histogram of "Ratio" in df
+    Uses pandas library
+    """
+    plt.style.use('seaborn-pastel')
+    kwargs = {"bins": tuple(range(0, 121, 20))}
+    ax = df0["Ratio"].hist(grid=False, **kwargs)
+    ax.set_title("Histogram of teach$Ratio")
+    ax.set_xlabel("teach$Ratio")
+    ax.set_ylabel("Frequency")
+    plt.show()
+    pass
+
+
 if __name__=="__main__":
     path = "/Users/joshuaelms/Desktop/github_repos/CSCI-B365_repo/CSCI-B365/HW0/Resources/teach.txt"
     df0 = q1_1(path)
     ratio_vals = q1_2(df0)
+    filtered_df = q1_3(df0)
+    q1_4(df0)
