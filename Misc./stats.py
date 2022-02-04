202b5c2eac8aeaa3124e77ff99ae10fbd3e7aca0
Joshua-Elms <bacon.joshy@gmail.com>
2022-02-04 17:16:21 -0500

    Implemented cov, var, corr, std

--- Misc./stats.py
+++ Misc./stats.py
@@ -1,14 +1,4 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 from math import sqrt
-
-def data():
-    df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases\
-/breast-cancer-wisconsin//breast-cancer-wisconsin.data",
- names=[f"V{i}" for i in range(1,12)])
-    df.index += 1
-    return df
 
 def cov(x: list|tuple, y: list|tuple, r: int = 9) -> float: 
     """
@@ -53,27 +43,3 @@ def std(x: list|tuple, r: int = 9) -> float:
     r is the desired numbers of digits after decimal to round to
     """
     return round(sqrt(var(x)), r)
-
-
-if __name__ == "__main__": 
-    df = data()
-    # q1 = df["V11"][df.V11 == 4].count()
-    # print(q1)
-
-    # print(df.isnull().count())
-
-    ### histograms ###
-    # print(df.describe())
-    # df.rename(columns={"V2": "Clump Thickness", "V3": "Uniformity of Cell Shape", "V4": "Uniformity of Cell Shape",
-    # "V5": "Marginal Adhesion", "V6": "Single Epithelial Cell Size", "V7": "Bare Nuclei", "V8": "Bland Chromatin", "V9": "Normed Nuclei", 
-    # "V10": "Mitoses", "V11": "Class (2 benign, 4 malignant)"}, inplace=True)
-    # print(df.head())
-    # df.hist(df.columns[1:]) # start at second column, first is ID field
-    # plt.show()
-
-    xlst, ylst = [1, 2, 3, 4], [10, 20, 30, 40]
-    r = 1
-    cov1, cov2, cov3 = corr(xlst, xlst, r), corr(xlst, ylst, r), corr(ylst, ylst, r)
-    print(f"[[{cov1}, {cov2}], \n[{cov2}, {cov3}]]")
-
-    # print(np.cov([xlst, ylst]))
