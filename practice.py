6f828056dd57fe6e3992dbf8f75107584a4f1822
Joshua-Elms <bacon.joshy@gmail.com>
2022-02-09 14:58:20 -0500

    No need for hstat import, line would throw error anyway

--- practice.py
+++ practice.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 # import pandas as pd
 # import sys
 # sys.path.insert(0, "/Users/joshuaelms/Desktop/github_repos/CSCI-B365_repo/CSCI-B365/Miscellaneous")
-import homemade_stats as hstat
+# import homemade_stats as hstat
 
 # t_points = [(0,0), (2,2), (4,4)]
 # df = pd.DataFrame(t_points, columns=("X", "Y"))
